6122243703de870b69128f738bdfc9927b8c253d
lorenamm <lorenamorais@alunos.utfpr.edu.br>
2023-01-15 13:09:46 -0300

    first commit 1

--- src/db.js
+++ src/db.js
@@ -2,10 +2,10 @@ import {MongoClient} from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoCilent = new MongoClient(process.env.MONGO_URI);
 
 await mongoClient.connect();
 
-const db = mongoClient.db('projeto12-batepapo-uol-api');
+const db = mongoClient.db('projeto13-batepapo-uol-api');
 
 export default db;
